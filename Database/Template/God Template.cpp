4e15c4c8e785976b490bd74b756f975633bb6a70
Pretisy <Pretisy@gmail.com>
2023-12-04 16:53:42 -0500

    Added in a God Template.
    Not made in code, but will be nice to have later.

---
+++ Database/Template/God Template.cpp
@@ -0,0 +1,86 @@
+
+
+
+
+
+Name:
+
+Pantheon: Arthurian, Babylonian, Celtic, Chinese, Egyptian, Great Old Ones, Greek, Hindu, Japanese, Maya, Norse, Polynesian, Roman, Slavic, Voodoo, Yoruba.
+Archetypes: Hunter, Guardian, Mage, Warrior, Assassin
+
+Type: Ranged, Melee
+Power Type: Physical, Magical
+
+Favor Cost:
+Release Date:
+
+// ====================================================== //
+
+Stats:
+
+	Health:
+		Health Per Level:
+
+	Mana:
+		Mana Per Level:
+
+	Speed:
+		Speed Per Level:
+
+	Range:
+		Range Per Level:
+
+	Attack Speed:
+		Attack Speed Per Level:
+
+
+
+Basic Attack:
+
+	Damage:
+		Damage Per Level:
+		Power Scale:
+
+	Progression:
+
+Protection:
+
+	Physical:
+		Physical Per Level:
+
+	Magical:
+		Magical Per Level:
+
+Regeneration:
+
+		Total HP5
+			Total HP5 Per level:
+
+		Total MP5:
+			Total MP5 Per Level:
+
+// ====================================================== //
+
+Abilities:
+
+		Passive:
+
+		1st Ability:
+			Cost:
+			Cooldown:
+			Ability Type:
+
+		2nd Ability:
+			Cost:
+			Cooldown:
+			Ability Type:
+
+		3rd Ability:
+			Cost:
+			Cooldown:
+			Ability Type:
+
+		Ultimate:
+			Cost:
+			Cooldown:
+			Ability Type:
