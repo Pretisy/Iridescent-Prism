745d08d1b1ddf746dbd132f3c9538b54cef214ef
Pretisy <Pretisy@gmail.com>
2023-12-05 17:03:55 -0500

    Added in a Item Template.
    I presume these are all the stats an item have; I will most likely re-arrange the list, or add any missing in the future.

--- Database/Template/God Template.cpp
+++ Database/Template/God Template.cpp
@@ -6,81 +6,81 @@
 Name:
 
 Pantheon: Arthurian, Babylonian, Celtic, Chinese, Egyptian, Great Old Ones, Greek, Hindu, Japanese, Maya, Norse, Polynesian, Roman, Slavic, Voodoo, Yoruba.
-Archetypes: Hunter, Guardian, Mage, Warrior, Assassin
+Archetypes : Hunter, Guardian, Mage, Warrior, Assassin
 
-Type: Ranged, Melee
-Power Type: Physical, Magical
+Type : Ranged, Melee
+Power Type : Physical, Magical
 
-Favor Cost:
-Release Date:
+Favor Cost :
+Release Date :
 
 // ====================================================== //
 
 Stats:
 
-	Health:
-		Health Per Level:
+Health:
+Health Per Level :
 
-	Mana:
-		Mana Per Level:
+Mana:
+Mana Per Level :
 
-	Speed:
-		Speed Per Level:
+Speed:
+Speed Per Level :
 
-	Range:
-		Range Per Level:
+Range:
+Range Per Level :
 
-	Attack Speed:
-		Attack Speed Per Level:
+Attack Speed :
+Attack Speed Per Level :
 
 
 
-Basic Attack:
+Basic Attack :
 
-	Damage:
-		Damage Per Level:
-		Power Scale:
+Damage:
+Damage Per Level :
+Power Scale :
 
-	Progression:
+Progression:
 
 Protection:
 
-	Physical:
-		Physical Per Level:
+Physical:
+Physical Per Level :
 
-	Magical:
-		Magical Per Level:
+Magical:
+Magical Per Level :
 
 Regeneration:
 
-		Total HP5
-			Total HP5 Per level:
+Total HP5
+Total HP5 Per level :
 
-		Total MP5:
-			Total MP5 Per Level:
+Total MP5 :
+Total MP5 Per Level :
 
 // ====================================================== //
 
 Abilities:
 
-		Passive:
+Passive:
 
-		1st Ability:
-			Cost:
-			Cooldown:
-			Ability Type:
+1st Ability :
+Cost:
+Cooldown:
+Ability Type :
 
-		2nd Ability:
-			Cost:
-			Cooldown:
-			Ability Type:
+2nd Ability :
+Cost:
+Cooldown:
+Ability Type :
 
-		3rd Ability:
-			Cost:
-			Cooldown:
-			Ability Type:
+3rd Ability :
+Cost:
+Cooldown:
+Ability Type :
 
-		Ultimate:
-			Cost:
-			Cooldown:
-			Ability Type:
+Ultimate:
+Cost:
+Cooldown:
+Ability Type :
---
+++ Database/Template/Item Template.cpp
@@ -0,0 +1,57 @@
+
+		
+
+
+
+		Name:
+			Item Tier:
+			Item Glyph:
+
+		Type:
+			Power Type: Physical, Magical
+
+			Total Gold Cost:
+				Gold Cost:
+
+			// ====================================================== //
+
+		Stats:
+
+			Health:
+			Mana:
+
+			Physical Power:
+			Magical Power:
+
+			Physical Lifesteal:
+			Magical Lifesteal:
+
+			Physical Flat Penetration:
+			Magical Flat Penetration:
+
+			Physical Percentage Penetration:
+			Magical Percentage Penetration:
+
+			Basic Attack Damage:
+			Attack Speed:
+			Critical Strike Chance:
+
+			Cooldown Reduction:
+			Crowd Control Reduction:
+
+			Physical Protection:
+			Magical Protection:
+
+			HP5 Regeneration:
+			MP5 Regeneration:
+			
+			GP5:
+			Speed:
+
+			Aura Effect:
+			Aura Range:
+
+			Passive Effect:
+			Passive Range:
+
+			// ====================================================== //
